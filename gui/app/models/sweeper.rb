2f1dd76aa41bb40f1e9f8f43762e045648062d24
Eric McCullough <eric_mccullough@hotmail.com>
2019-01-26 22:27:24 -0500

    Another file for MAC upcase and de-punc

--- gui/app/models/sweeper.rb
+++ gui/app/models/sweeper.rb
@@ -1,11 +1,17 @@
 class Sweeper < ActiveRecord::Base
   require 'resolv'
   self.per_page = 10
-  VALID_MAC_REGEX = /\A[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}\z/i
+  #VALID_MAC_REGEX = /\A[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}\z/i
+  VALID_MAC_REGEX = /\A[\da-f]{12}\z/i
   validates :mac, presence: true,
                   format: { with: VALID_MAC_REGEX },
                   uniqueness: { case_sensitive: false }
   validates :ip, format: { :with => Resolv::IPv4::Regex },
                  allow_nil: true,
                  allow_blank: true
+
+  def mac=(val)
+    write_attribute :mac, val.upcase.gsub(/[-:\.]/,'')
+  end
+  
 end
