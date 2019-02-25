fb4d2634043f5020177eee5255906fd629c0c83f
Eric McCullough <eric_mccullough@hotmail.com>
2019-02-25 07:52:22 -0500

    Add logging for new lease/device to lease model

--- gui/app/models/lease.rb
+++ gui/app/models/lease.rb
@@ -9,4 +9,20 @@ class Lease < ActiveRecord::Base
   belongs_to :device, dependent: :destroy
   accepts_nested_attributes_for :device
   self.per_page = 10
+  
+  after_create do
+    begin
+      if self.device.created_at.to_i >= (self.created_at.to_i - 1)
+        foundb4 = 'N'
+      else
+        foundb4 = 'Y'
+      end
+      vendor = Vendor.find_by(oui: self.device.mac[0..5]).name
+      logger.info "\{'r2d2':\{'sensor':'d1s2','scope':'#{self.scope.ip}',"+
+                  "'mac':'#{self.device.mac}','dhcp-host':'#{self.name}',"+
+                  "'IP':'#{self.ip}','Timestamp':'#{self.created_at}',"+
+                  "'Found_before':'#{foundb4}','Lease':'','Vendor':'#{vendor}'\}\}"
+    rescue
+    end
+  end
 end
